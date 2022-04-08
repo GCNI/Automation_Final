c0582149738545255931259600ea0eb9e3b515f1
GCNI <102343182+GCNI@users.noreply.github.com>
2022-04-08 16:39:53 +0530

    Update wdio.conf.js
    added file path

--- wdio.conf.js
+++ wdio.conf.js
@@ -21,7 +21,7 @@ exports.config = {
     // will be called from there.
     //
     specs: [
-        './test/specs/**/loginThroughCernerWiki.wdio.js'
+        './test/specs/**/*.js'
     ],
     // Patterns to exclude.
     exclude: [
